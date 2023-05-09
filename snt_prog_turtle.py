8da56188d36f4041933c6760b32ad6010553ec08
Lier0 <28351075+Lier0@users.noreply.github.com>
2023-05-09 09:07:13 +0200

    snt: internet, tp plus progressif

---
+++ snt_prog_turtle.py
@@ -0,0 +1,37 @@
+import turtle as t
+"""
+x,y = 0, 0
+while True:
+    t.forward(60)
+    t.left(120)
+    t.forward(60)
+    t.right(90)
+    t.circle(30, 300)
+    t.right(90)
+    t.forward(60)
+    x += 10
+    y += 1
+    t.goto(x, y)
+
+"""
+
+t.width(1)
+t.fillcolor("yellow")
+t.begin_fill()
+t.left(90)
+t.forward(100)
+t.right(120)
+t.forward(200)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(200)
+t.end_fill()
+"""
+t.end_fill()
+t.goto(5, 5)
+t.goto(55, 105)
+t.goto(5, 105)
+t.goto(55, 5)
+"""
+t.exitonclick()
